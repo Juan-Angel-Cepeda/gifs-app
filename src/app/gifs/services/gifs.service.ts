08590792690e13fb694552cb3ef82d06b16a34d8
angelcepeda <jangelcepeda@protonmail.com>
2023-04-04 16:51:53 -0700

    http methods agregated in the gif service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,21 +7,28 @@ import { Injectable } from '@angular/core';
 
 export class GifsService {
 
+  private apiKey: string = 'cTkNCVXfXj0jdpgfM7H3QGQt9ZqtkWXg';
   private _historial: string[] = [];
 
   get historial(){
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient){}
+
   buscarGifs(query:string = ''){
     
     query = query.trim().toLowerCase();
+    
     if(!this._historial.includes(query)){
       this._historial.unshift( query );
+      this._historial  = this._historial.splice(0,10);
     }
 
-    this._historial  = this._historial.splice(0,10);
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=cTkNCVXfXj0jdpgfM7H3QGQt9ZqtkWXg&q=planets&limit=10&offset=0&rating=g&lang=en')
+    .subscribe((resp:any )=> {
+      console.log(resp.data);
+    });
   
   }
 

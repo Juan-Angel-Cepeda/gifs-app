5a19f334e722968bd8d6115def28b59940480281
angelcepeda <jangelcepeda@protonmail.com>
2023-04-04 15:39:04 -0700

    gifs service working, save in an array the gifs searched

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild,ElementRef } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -10,9 +11,11 @@ export class BusquedaComponent{
 
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor(private GifsService:GifsService){}
+  
   buscar( ){
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    this.GifsService.buscarGifs(valor)
     this.txtBuscar.nativeElement.value = '';
   }
 
